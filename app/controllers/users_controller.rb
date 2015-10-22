c78105591448726d4a8de12e43ae59df6ea126ce
Justin Scott & Austin Kincaid <student@epicodus.com>
2015-10-22 10:45:50 -0700

    Give admins privileges to delete users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,30 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(current_user.id)
+    @users = User.all
+    @photos = Photo.all
+    @comments = Comment.all
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    flash[:danger] = "User successfully deleted"
+    redirect_to user_path(current_user)
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.admin
+      @user.remove_admin
+      @user.save
+      flash[:success] = "#{@user.user_name} is no longer an admin"
+      redirect_to user_path(current_user)
+    else
+      @user.make_user_admin
+      @user.save
+      redirect_to user_path(current_user)
+      flash[:success] = "#{@user.user_name} is now an admin"
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,23 @@ class User < ActiveRecord::Base
   has_many :tagged_users
   has_many :photo_tagged, class_name: "Photo", foreign_key: "photo_id", through: :tagged_users
 
+  before_save :make_admin
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def make_user_admin
+    self.admin = true
+  end
+
+  def remove_admin
+    self.admin = false
+  end
+
+private
+  def make_admin
+    if User.all.length < 2
+      self.admin = true
+    end
+  end
 end
