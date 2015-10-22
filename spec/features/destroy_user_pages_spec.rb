b956afb7a422e7006a1ddc42c33c68b43995a3ad
Justin Scott & Austin Kincaid <student@epicodus.com>
2015-10-22 15:17:55 -0700

    Add working specs for deleting users

--- spec/features/destroy_user_pages_spec.rb
+++ spec/features/destroy_user_pages_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
 describe "User sign-up and login path" do
-  # admin = FactoryGirl.create(:user)
-  # user = FactoryGirl.create(:no_admin_user)
-  #
-  # it "it allows an admin to delete another user" do
-  #   visit '/'
-  #   click_on "Login"
-  #   fill_in 'Email', :with => admin.email
-  #   fill_in 'Password', :with => admin.password
-  #   click_button 'Log in'
-  #   # save_and_open_page
-  #   click_on admin.user_name
-  #   click_link('user-delete')
-  #   expect(page).not_to have_content user.user_name
-  # end
+
+  it "it allows an admin to delete another user" do
+    user = FactoryGirl.create(:no_admin_user)
+    admin = FactoryGirl.create(:user)
+    visit '/'
+    click_on "Login"
+    fill_in 'Email', :with => admin.email
+    fill_in 'Password', :with => admin.password
+    click_button 'Log in'
+    # save_and_open_page
+    click_on admin.user_name
+    click_link('user-delete', match: :first)
+    expect(page).not_to have_content user.user_name
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,12 +10,13 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
   config.infer_spec_type_from_file_location!
 
   # database_cleaner information
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :truncation
+  config.before :suite do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
   end
   config.before(:each) do
     DatabaseCleaner.start
