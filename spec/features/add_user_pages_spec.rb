0fd14b237d491a29cdec68227b3939e3a16dfc8b
Justin Scott & Austin Kincaid <student@epicodus.com>
2015-10-22 14:18:13 -0700

    Add spec for user login and profile view

--- spec/features/add_user_pages_spec.rb
+++ spec/features/add_user_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe "User sign-up and login path" do
+  user = FactoryGirl.create(:user)
   it "adds a user and logs them in" do
     visit '/'
     click_on "Sign up"
@@ -10,5 +11,15 @@ describe "User sign-up and login path" do
     fill_in 'Password confirmation', :with => "testtest"
     click_button 'Sign up'
     expect(page).to have_content "Welcome! You have signed up successfully."
+  end
+
+  it "it allows a user to signin and view their profile page" do
+    visit '/'
+    click_on "Login"
+    fill_in 'Email', :with => user.email
+    fill_in 'Password', :with => user.password
+    click_button 'Log in'
+    click_on user.user_name
+    expect(page).to have_content "Admin Privileges"
   end
 end
